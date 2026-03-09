4541823b834fac9d952f6ff93049c3109f04fa4a
CIS LAB <cislab112@ad.nwscc.edu>
2026-03-09 10:29:20 -0500

    iss2: Search Bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,25 @@
-// placeholder
+function searchBooks() {
+    const books = [
+        "Twilight",
+        "The Hunger Games",
+        "Harry Potter"
+        "The Wizard of Oz",
+        "The Notebook",
+        "The Great Gatsby" 
+    ];
+
+
+const input = document.getElementById("searchInput").value.toLowerCase();
+const resultsDiv = document.getElementById("results");
+
+const filterBooks = books.filter(book =>
+    book.toLowerCase().includes(input)
+);
+
+if (filteredBooks.length > 0 && input !=="") {
+    resultsDiv.innerHTML = "Results found: " + filteredBooks.join(", ");
+} else {
+    resultsDiv.innerHTML = "No results found. ";
+}
+
+}
